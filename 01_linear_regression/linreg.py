d295284f9f4de6adf9d0fe9f46a483c001b3a0d8
kaustubhn <kaustubhnarkhede88@gmail.com>
2017-03-10 22:58:07 +0530

    Updated linreg to add filewriter and add graph

--- 01_linear_regression/linreg.py
+++ 01_linear_regression/linreg.py
@@ -5,25 +5,25 @@ from sklearn.datasets import load_diabetes, load_boston
 from sklearn.model_selection import train_test_split
 
 def get_data():
-    # Replace the below function call to apply linear regression on different dataset
-    boston = load_boston()
-    features = np.array(boston.data)
-    labels = np.array(boston.target)
-    return features, labels
+	# Replace the below function call to apply linear regression on different dataset
+	boston = load_boston()
+	features = np.array(boston.data)
+	labels = np.array(boston.target)
+	return features, labels
 
 def normalize(dataset):
 	# Check details here https://en.wikipedia.org/wiki/Feature_scaling
-    mu = np.mean(dataset,axis=0)
-    sigma = np.std(dataset,axis=0)
-    return (dataset - mu)/sigma
+	mu = np.mean(dataset,axis=0)
+	sigma = np.std(dataset,axis=0)
+	return (dataset - mu)/sigma
 
 def add_bias_reshape(features,labels):
 	# Add b term of the equation Y = W * X + b, which will be all 1's
-    n_training_samples = features.shape[0]
-    n_dim = features.shape[1]
-    f = np.reshape(np.c_[np.ones(n_training_samples),features],[n_training_samples,n_dim + 1])
-    l = np.reshape(labels,[n_training_samples,1])
-    return f, l
+	n_training_samples = features.shape[0]
+	n_dim = features.shape[1]
+	f = np.reshape(np.c_[np.ones(n_training_samples),features],[n_training_samples,n_dim + 1])
+	l = np.reshape(labels,[n_training_samples,1])
+	return f, l
 
 
 features,labels = get_data()
@@ -37,9 +37,9 @@ learning_rate = 0.01
 training_epochs = 1000
 loss_history = np.empty(shape=[1], dtype=float)
 
-X = tf.placeholder(tf.float32, [None, n_dim])
-Y = tf.placeholder(tf.float32, [None, 1])
-W = tf.Variable(tf.ones([n_dim, 1]))
+X = tf.placeholder(tf.float32, [None, n_dim], name= "X")
+Y = tf.placeholder(tf.float32, [None, 1], name = "Y")
+W = tf.Variable(tf.ones([n_dim, 1]), name = "W")
 
 
 init = tf.global_variables_initializer()
@@ -50,8 +50,11 @@ loss = tf.reduce_mean(tf.square(y_-Y))
 
 train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
+
+
 with tf.Session() as sess:
 	sess.run(init)
+	writer = tf.summary.FileWriter('./graph', sess.graph)
 	for epoch in range(training_epochs):
 		sess.run(train_step, feed_dict={X:X_train, Y:Y_train})
 		loss_history = np.append(loss_history, sess.run(loss, feed_dict={X:X_train, Y:Y_train}))
@@ -59,7 +62,7 @@ with tf.Session() as sess:
 
 	mse = tf.reduce_mean(tf.square(y_pred - y_test))
 	print sess.run(mse)
-
+writer.close()
 # Plot for Measure vs Predicted target value
 fig, ax = plt.subplots()
 ax.scatter(y_test, y_pred)
